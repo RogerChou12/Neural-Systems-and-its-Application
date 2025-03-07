f98f4e4c74318bec0a983688c2586f7ee9ac385b
RogerChou12 <yi.chen.chou.work@gmail.com>
2025-03-07 08:31:22 +0000

    add comments and rename variables

--- ART/HW04_ART.py
+++ ART/HW04_ART.py
@@ -16,48 +16,49 @@ for f in range(16):
            if x[f,(7*i)+y]==1:   # If the value is 1, plot a square
             # In image representation, the top-left corner is (0,0) in the matrix.
             # But the origin(0,0) of Matplotlib's default coordinate system is at bottom-left.
-             plt.scatter(y+1,10-i,color='red',s=50,marker='s')  
+            plt.scatter(y+1,10-i,color='red',s=50,marker='s')  
     plt.axis([0,7.5,0,10.5])  #x-axis(min,max) y-axis(min,max)
 
 w=np.zeros([20,70]) # Stores learned cluster centers (max 20 clusters)
-w[0,:]=x[0,:]
+w[0,:]=x[0,:] # Initialize the first cluster with the first character's pattern
 epoch=0
 thr=0.8   # thr=0.4 0.6 0.8 # Similarity threshold (higher = stricter clustering)
-w_count=1 # Number of clusters
-epoch_t=8 # Training epochs
-while epoch < epoch_t:    
-    for t in range(16):
-        w_tid=np.zeros(w_count)
-        for f in range(w_count):
+clusters=1 # Number of clusters
+epoch_total=8 # Training epochs
+while epoch < epoch_total:    
+    for character in range(16):
+        w_tid=np.zeros(clusters)
+        for f in range(clusters):
             w_tid[f]=1/(sum(w[f,:])+0.5)
-        y=np.zeros(w_count)
-        y_1=np.ones(w_count)
-        test=0
+        y=np.zeros(clusters) # Cluster activation values
+        y_1=np.ones(clusters) # Activation status for clusters
+        assigned=0 # determine if a character is assigned to a cluster
         count=0
-        while test ==0:
-            for time in range(w_count):
-                y[time]=sum(w[time,:]*x[t,:])*w_tid[time]*y_1[time]
+        while assigned ==0:
+            for cluster in range(clusters):
+                y[cluster]=sum(w[cluster,:]*x[character,:])*w_tid[cluster]*y_1[cluster]
             
             max_index=np.argmax(y) # The most similar cluster
-            v=sum(w[max_index,:]*x[t,:])/sum(x[t,:])   # Similarity score
+            v=sum(w[max_index,:]*x[character,:])/sum(x[character,:])   # Similarity score
 
             if v >= thr: # Merges the character into the cluster if similarity is high enough
-                w[max_index,:]=w[max_index,:]*x[t,:]
-                test=1
-                if epoch ==(epoch_t-1): # If it's the final epoch, the character is added to 'drawn_tit'
-                    drawn_tit[max_index]+=tit[t]
-            else:
-                y_1[max_index]=0
+                w[max_index,:]=w[max_index,:]*x[character,:] # Update weights
+                assigned=1
+                if epoch ==(epoch_total-1): # If it's the final epoch, the character is added to 'drawn_tit'
+                    drawn_tit[max_index]+=tit[character]
+            else: # Similarity score is too low
+                y_1[max_index]=0 # Temporarily deactivate the cluster
+            
             if sum(y)==0: # If no suitable cluster is found, a new cluster is created
-                w[w_count,:]=x[t,:]
-                if epoch ==(epoch_t-1): 
-                    drawn_tit[w_count]+=tit[t]
-                w_count+=1
-                test=1
+                w[clusters,:]=x[character,:]
+                if epoch ==(epoch_total-1): 
+                    drawn_tit[clusters]+=tit[character]
+                clusters+=1
+                assigned=1
     epoch+=1
     print(epoch)
 print(drawn_tit)    
-for f in range(w_count):
+for f in range(clusters):
     plt.figure(figsize=(3.5,5))
     #figure tilte顯示中文字
     plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] # Set font for Chinese text
